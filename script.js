a74e8390f4a28d077b4a77aa451fa89f5cc95e79
deejayjam27 <108850989+deejayjam27@users.noreply.github.com>
2024-01-28 00:39:41 +0800

    last-commit
    01/28/2024

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ window.onclick = function(e){
         }
     }
 }
+var coll = document.getElementsByClassName("collapsible");
+var i;
+for(i = 0; i < coll.length; i++){
+    coll[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block"){
+            content.style.display = "none";
+        }else{
+            content.style.display = "block";
+        }
+    });
+}
 
 hamburger.addEventListener("click", () =>{
     hamburger.classList.toggle("active");
